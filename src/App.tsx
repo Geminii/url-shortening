0c7b0200ab2ce290ee0df61076c5bfe16e3f0769
Jimmy Jouanne <jimmy@liberkeys.com>
2022-01-13 18:07:17 +0100

    feat: add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,10 @@ function App() {
           Learn React
         </a>
       </header>
+
+      <h1 className="text-3xl font-bold underline text-emerald-700">
+        Hello world!
+      </h1>
     </div>
   );
 }
